eddf06fa6fe1bda3da5403b0e241d0fe5adf5136
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Korean translations for candy-wish.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'middleware.cannot_open_stderr' => 'php://stderr을(를) 열 수 없음',
+    'middleware.stderr_not_resource' => 'stderr은 리소스여야 합니다',
+    'logger.cannot_open_target'      => '로그 대상을 열 수 없음: {target}',
+    'logger.invalid_target'          => '로거 대상은 경로, 리소스 또는 null이어야 합니다',
+    'bubbletea.bad_factory'          => 'BubbleTea 팩터리는 run() 메서드가 있는 객체를 반환해야 합니다; 받음: {got}',
+];
